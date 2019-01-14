bbe5937e1926efbb9c52f25b53f3ecef34a7c5c8
Julien <julien.fallon@imt-atlantique.net>
2019-01-14 15:06:51 +0100

    reorganisation et creation et sauvegarde txt des matrices

--- performance400/StereoFindExtrinsicParameters.py
+++ performance400/StereoFindExtrinsicParameters.py
@@ -1,62 +1,67 @@
-
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-cap = cv2.VideoCapture('videos/agnesModel.MOV')
-success, frame = cap.read()
-cv2.imwrite('images/photosPisteCali.jpg', frame)
+frame1 = cv2.imread('images/photosMire/IMG_2101.jpg')
+frame2 = cv2.imread('images/photosMire/IMG_2102.jpg')
 
-obj_points = [[0, 0, 0], [0, 1.22, 0], [0, 2.44, 0], [0, 3.66, 0], [0, 4.88, 0], [0, 6.1, 0], [0, 7.32, 0],
-              [20, 1.22, 0], [20, 2.44, 0], [20, 3.66, 0], [20, 4.88, 0], [20, 6.1, 0], [20, 7.32, 0],
-              [40, 1.22, 0], [40, 2.44, 0], [40, 3.66, 0], [40, 4.88, 0], [40, 6.1, 0], [40, 7.32, 0],
-              [50, 1.22, 0], [50, 2.44, 0], [50, 3.66, 0], [50, 4.88, 0], [50, 6.1, 0], [50, 7.32, 0]]
-
-img_points1 = [[1572, 1534], [1640, 1557], [1708, 1578], [1780, 1601], [1853, 1628], [1925, 1651], [1997, 1674], [2036, 768],
-              [2094, 783], [2153, 799], [2213, 813], [2274, 828], [2334, 845], [2309, 226], [2361, 237],
-              [2411, 247], [2463, 260], [2513, 271], [2566, 281], [2416, 15], [2464, 24], [2509, 36], [2557, 45],
-              [2607, 56], [2655, 66]]
-
-img_points2 = [[1572, 1534], [1640, 1557], [1708, 1578], [1780, 1601], [1853, 1628], [1925, 1651], [1997, 1674], [2036, 768],
-              [2094, 783], [2153, 799], [2213, 813], [2274, 828], [2334, 845], [2309, 226], [2361, 237],
-              [2411, 247], [2463, 260], [2513, 271], [2566, 281], [2416, 15], [2464, 24], [2509, 36], [2557, 45],
-              [2607, 56], [2655, 66]]
-
-np.savetxt('matrices/obj_points_testStereo1', obj_points)
-np.savetxt('matrices/img_points_testStereo_cam1', img_points1)
-np.savetxt('matrices/img_points_testStereo_cam2', img_points2)
+obj_points = np.loadtxt('matrices/obj_points_stereo_test1')
+img_points1 = np.loadtxt('matrices/img_points3_stereo_test1')
+img_points2 = np.loadtxt('matrices/img_points4_stereo_test1')
 
 obj_points = np.array(obj_points, 'float32')
-img_points = np.array(img_points1, 'float32')
-img_points = np.array(img_points2, 'float32')
-size = frame.shape[:2]
+img_points1 = np.array(img_points1, 'float32')
+img_points2 = np.array(img_points2, 'float32')
+size = frame2.shape[:2]
 
 camera_matrix = np.loadtxt('matrices/camera_matrix')
 dist_coefs = np.loadtxt('matrices/distortion_vector')
+R = np.array((3, 3), 'float32')
+T = np.array((3, 3), 'float32')
 
-retval, camera_matrix, dist_coefs, rvecs, tvecs = cv2.calibrateCamera([obj_points], [img_points], size, camera_matrix,
-                                                                      dist_coefs,
-                                                                      flags=cv2.CALIB_USE_INTRINSIC_GUESS)
+retval, camera_matrix1, dist_coefs1, rvecs, tvecs = cv2.calibrateCamera([obj_points], [img_points1], size,
+                                                                        camera_matrix, dist_coefs,
+                                                                        flags=cv2.CALIB_USE_INTRINSIC_GUESS)
 
-rotation_matrix, jacobian = cv2.Rodrigues(rvecs[0])
+retval, camera_matrix2, dist_coefs2, rvecs, tvecs = cv2.calibrateCamera([obj_points], [img_points2], size,
+                                                                        camera_matrix, dist_coefs,
+                                                                        flags=cv2.CALIB_USE_INTRINSIC_GUESS)
 
-np.savetxt('matrices/adjusted_camera_matrix', camera_matrix)
-np.savetxt('matrices/adjusted_distortion_vector', dist_coefs)
-np.savetxt('matrices/rotation_vector', rvecs[0])
-np.savetxt('matrices/translation_vector', tvecs[0])
-np.savetxt('matrices/rotation_matrix', rotation_matrix)
+retval, camera_matrix1, dist_coefs1, \
+camera_matrix2, dist_coefs2, R, T, E, F = cv2.stereoCalibrate([obj_points], [img_points1], [img_points2],
+                                                              camera_matrix1, dist_coefs1, camera_matrix2, dist_coefs2,
+                                                              size, flags=cv2.CALIB_FIX_INTRINSIC)
 
-mean_error = 0
-# dessiner les axes
-frame = cv2.drawFrameAxes(frame, camera_matrix, dist_coefs, rvecs[0], tvecs[0], 100)
-cv2.imwrite('images/frame1_axes.jpg', frame)
+R1, R2, P1, P2, Q, validPixROI1, validPixROI2 = cv2.stereoRectify(camera_matrix1, dist_coefs1, camera_matrix2,
+                                                                  dist_coefs2, size, R, T,
+                                                                  flags=cv2.CALIB_ZERO_DISPARITY)
 
-# redessiner les points source et calculer le pourcentage d'erreur comis
-img_points2, jacobian = cv2.projectPoints(obj_points, rvecs[0], tvecs[0], camera_matrix, dist_coefs)
-for px in img_points2:
-    cv2.circle(frame, (px[0][0], px[0][1]), 10, (255, 255, 255), 5)
-cv2.imwrite('images/frame1_errors.jpg', frame)
+np.savetxt('matrices/camera_matrix1_stereo_test1', camera_matrix1)
+np.savetxt('matrices/camera_matrix2_stereo_test1', camera_matrix2)
+np.savetxt('matrices/distortion_vector1_stereo_test1', dist_coefs1)
+np.savetxt('matrices/distortion_vector2_stereo_test1', dist_coefs2)
+np.savetxt('matrices/R_stereo_test1', R)
+np.savetxt('matrices/T_stereo_test1', T)
+np.savetxt('matrices/E_stereo_test1', E)
+np.savetxt('matrices/F_stereo_test1', F)
 
-for i in range(0, len(img_points2)):
-    mean_error += cv2.norm(img_points[i], img_points2[i][0], cv2.NORM_L2) / len(img_points2)
-print('erreur moyenne', mean_error)
+# I3 = np.identity(3, 'float32')
+# Zeros = np.zeros((3, 1), 'float32')
+
+# P1 = np.append(I3, Zeros, axis=1)
+# P2 = np.append(R, T, axis=1)
+
+img_points1 = np.array(img_points1, 'float32')
+img_points2 = np.array(img_points2, 'float32')
+
+points4D = cv2.triangulatePoints(P1, P2, np.array(img_points1)[0], np.array(img_points2)[0])
+
+print(points4D)
+np.savetxt('matrices/points4D_stereo_test1', points4D)
+
+point3D = np.array((1, 3), 'float32')
+point3D = cv2.convertPointsFromHomogeneous(points4D)
+
+print(point3D)
+np.savetxt('matrices/points4D_stereo_test1', point3D)
 
