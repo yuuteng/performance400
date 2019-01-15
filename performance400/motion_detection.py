62757605ed6f315db331b978d15ed939f6bb77b0
Thomas Schillaci <thomas.schillaci@gmail.com>
2019-01-15 14:42:28 +0100

    motion detection improvements

--- performance400/motion_detection.py
+++ performance400/motion_detection.py
@@ -6,20 +6,22 @@ import scipy.signal
 from performance400.calculate_3d_coords import calculate_3d_coords
 
 
+# Détermine les formes qui ont changé par rapport à background
 def get_frames(m_frame, m_background):
     m_gray_frame = cv2.cvtColor(m_frame, cv2.COLOR_BGR2GRAY)
-    m_gray_frame = cv2.GaussianBlur(m_gray_frame, (21, 21), 0)
+    m_gray_frame = cv2.GaussianBlur(m_gray_frame, (GAUSSIAN_BLUR, GAUSSIAN_BLUR), 0)
 
     if m_background is None:
         m_background = m_gray_frame
 
     m_difference_frame = cv2.absdiff(m_background, m_gray_frame)
-    m_threshold_frame = cv2.threshold(m_difference_frame, 10, 255, cv2.THRESH_BINARY)[1]
-    m_threshold_frame = cv2.dilate(m_threshold_frame, None, iterations=2)
+    m_threshold_frame = cv2.threshold(m_difference_frame, DETECTION_THRESHOLD, 255, cv2.THRESH_BINARY)[1]
+    m_threshold_frame = cv2.dilate(m_threshold_frame, None, iterations=NUMBER_OF_DILATATION)
 
     return m_gray_frame, m_difference_frame, m_threshold_frame, m_background
 
 
+# Renvoie le plus grand contours de la collection m_contours
 def get_largest_contour(m_contours):
     m_largest_contour = contours[0]
 
@@ -30,6 +32,7 @@ def get_largest_contour(m_contours):
     return m_largest_contour
 
 
+# Renvoie le centre du contour m_largest_contour et le dessine sur m_frame
 def draw_position(m_largest_contour, m_frame):
     (x, y, w, h) = cv2.boundingRect(m_largest_contour)
     x1, y1 = x, y
@@ -40,6 +43,7 @@ def draw_position(m_largest_contour, m_frame):
     return (x1 + x2) / 2, (y1 + y2) / 2, m_frame
 
 
+# Dessine la trajectoire du centre du plus grand contour sur m_frame
 def draw_trajectory(m_trajectory, m_frame, color=(0, 255, 0)):
     cv2.polylines(m_frame, [np.array(m_trajectory, 'int32').reshape((-1, 1, 2))], isClosed=False, color=color,
                   thickness=10, lineType=cv2.LINE_AA)
@@ -47,6 +51,7 @@ def draw_trajectory(m_trajectory, m_frame, color=(0, 255, 0)):
     return m_frame
 
 
+# Lisse la trajectoire m_trajectory
 def trajectory_smoothing(m_trajectory, m_window_length, m_threshold):
     m_consecutive_squared_distances = [0]
     for j in range(1, len(m_trajectory)):
@@ -64,6 +69,7 @@ def trajectory_smoothing(m_trajectory, m_window_length, m_threshold):
     return m_corrected_trajectory
 
 
+# Filtre la trajectoire m_trajectory
 def trajectory_filtering(m_trajectory):
     m_shaped_trajectory = np.transpose(np.asarray(m_trajectory))
     m_filtered_x = scipy.signal.savgol_filter(m_shaped_trajectory[0], 21, 5)
@@ -76,12 +82,19 @@ def trajectory_filtering(m_trajectory):
 
 FIRST_FRAME_INDEX = 0
 LAST_FRAME_INDEX = 210
+VIDEO_FREQUENCY = 30
+DETECTION_THRESHOLD = 10
+MIN_CONTOUR_AREA = 2000
+GAUSSIAN_BLUR = 21
+NUMBER_OF_DILATATION = 2
+
+video = cv2.VideoCapture('videos/runway/gauche.mp4')
 
 background = None
 trajectory = []
 
-video = cv2.VideoCapture('videos/runway/gauche.mp4')
 for i in range(-FIRST_FRAME_INDEX, LAST_FRAME_INDEX):
+    # On s'assure que la frame courante est bonne et nous intéresse
     frame = video.read()[1]
     if frame is None:
         break
@@ -89,19 +102,22 @@ for i in range(-FIRST_FRAME_INDEX, LAST_FRAME_INDEX):
     if i < 0:
         continue
 
+    # On récupère les formes en mouvement
     gray_frame, difference_frame, threshold_frame, background = get_frames(frame, background)
 
+    # On détermine leurs contours
     contours = cv2.findContours(threshold_frame.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[1]
 
     if contours is not None:
         if len(contours) > 0:
+            # On récupère la plus grande forme, et si elle est assez grande, on dessine son contour, on détermine son
+            # centre et on calcule sa trajectoire
             largest_contour = get_largest_contour(contours)
 
-            if cv2.contourArea(largest_contour) > 2000:
+            if cv2.contourArea(largest_contour) > MIN_CONTOUR_AREA:
                 xc, yc, frame = draw_position(largest_contour, frame)
                 xcc, ycc = calculate_3d_coords(xc, yc)[:2]
-                # trajectory.append((xcc[0], ycc[0]))
-                trajectory.append((xc, yc))
+                trajectory.append((xcc[0], ycc[0]))
                 # frame = draw_trajectory(trajectory, frame)
     else:
         trajectory.append((None, None))
@@ -122,22 +138,24 @@ for i in range(-FIRST_FRAME_INDEX, LAST_FRAME_INDEX):
 video.release()
 cv2.destroyAllWindows()
 
-
+# On lisse la trajectoire
 trajectory = trajectory_smoothing(trajectory, 10, 3)
-time = np.linspace(0, 7 * len(trajectory) / (LAST_FRAME_INDEX - FIRST_FRAME_INDEX), len(trajectory))
-velocity = [np.linalg.norm(np.asarray(trajectory[i]) - np.asarray(trajectory[i - 1])) for i in
+time = np.linspace(0, len(trajectory) / VIDEO_FREQUENCY, len(trajectory))
+# On déduit la vitesse de la forme en mouvement à partir de sa trajectoire lissée
+velocity = [np.linalg.norm(np.asarray(trajectory[i]) - np.asarray(trajectory[i - 1])) * VIDEO_FREQUENCY for i in
             range(1, len(trajectory))]
 
+# On représente les données obtenues
 plot.subplot(2, 1, 1)
 plot.title("Profil de position")
 plot.xlabel("Temps (s)")
-plot.ylabel("Position")
+plot.ylabel("Position (m)")
 lines = plot.plot(time, trajectory)
 plot.legend([lines[0], lines[1]], ["Position suivant x", "Position suivant y"])
 
 plot.subplot(2, 1, 2)
 plot.title("Profil de vitesse")
 plot.xlabel("Temps (s)")
-plot.ylabel("Vitesse")
+plot.ylabel("Vitesse (m/s)")
 plot.plot(time[:-1], velocity)
 plot.show()
