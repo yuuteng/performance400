d9efb514f5e8771f4a562f6cac760a3de81026e1
Julien <julien.fallon@imt-atlantique.net>
2019-01-16 09:35:09 +0100

    nouveau fichiers intrin et extrin + stereo 3d

---
+++ performance400/find_3D_coords_stereo.py
@@ -0,0 +1,114 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+
+def find_3d_coords_stereo(img_gauche, img_droite, obj_points, img_points_gauche, img_points_droite,
+                          camera_matrix_gauche, camera_matrix_droite, dist_coeffs_gauche, dist_coeffs_droite,
+                          show=False, save=False, prefix='', rvec_gauche=None, tvec_gauche=None, rvec_droite=None,
+                          tvec_droite=None):
+    obj_points = np.array(obj_points, 'float32')
+    img_points_gauche = np.array(img_points_gauche, 'float32')
+    img_points_droite = np.array(img_points_droite, 'float32')
+    size = img_gauche.shape[:2]
+
+    R = np.eye(3)
+    T = np.eye(3)
+    R1 = np.zeros(shape=(3, 3))
+    R2 = np.zeros(shape=(3, 3))
+    P1 = np.zeros(shape=(3, 3))
+    P2 = np.zeros(shape=(3, 3))
+
+    stereocalib_criteria = (cv2.TERM_CRITERIA_MAX_ITER + cv2.TERM_CRITERIA_EPS, 100, 1e-5)
+    stereocalib_flags = cv2.CALIB_FIX_ASPECT_RATIO | cv2.CALIB_ZERO_TANGENT_DIST | cv2.CALIB_SAME_FOCAL_LENGTH | \
+                        cv2.CALIB_RATIONAL_MODEL | cv2.CALIB_FIX_K3 | cv2.CALIB_FIX_K4 | cv2.CALIB_FIX_K5
+
+    retval, camera_matrix_gauche, dist_coeffs_gauche, \
+    camera_matrix_droite, dist_coeffs_droite, R, T, E, F = cv2.stereoCalibrate([obj_points], [img_points_gauche],
+                                                                               [img_points_droite],
+                                                                               camera_matrix_gauche, dist_coeffs_gauche,
+                                                                               camera_matrix_droite, dist_coeffs_droite,
+                                                                               size, flags=stereocalib_flags,
+                                                                               criteria=stereocalib_criteria)
+
+    R1, R2, P1, P2, Q, validPixROI1, validPixROI2 = cv2.stereoRectify(camera_matrix_gauche, dist_coeffs_gauche,
+                                                                      camera_matrix_droite, dist_coeffs_droite,
+                                                                      size, R, T, R1, R2, P1, P2, alpha=1)
+
+    points4D = cv2.triangulatePoints(P1, P2, img_points_gauche.T, img_points_droite.T)
+    points3D = cv2.convertPointsFromHomogeneous(points4D.T)
+
+    if save:
+        if prefix == '':
+            return print('Veuillez saisir un prefix')
+        points3D_bis = []
+        for punkt in points3D:
+            points3D_bis.append([punkt[0][0], punkt[0][1], punkt[0][2]])
+        np.savetxt('matrices/camera_matrix/stereo_calib/' + prefix + '_gauche_camera_matrix', camera_matrix_gauche)
+        np.savetxt('matrices/camera_matrix/stereo_calib/' + prefix + '_droite_camera_matrix', camera_matrix_droite)
+        np.savetxt('matrices/vectors/distortion/stereo_calib/' + prefix + '_gauche_distortion_vector',
+                   dist_coeffs_gauche)
+        np.savetxt('matrices/vectors/distortion/stereo_calib/' + prefix + '_droite_distortion_vector',
+                   dist_coeffs_droite)
+        np.savetxt('matrices/stereo_calib/' + prefix + '_R', R)
+        np.savetxt('matrices/stereo_calib/' + prefix + '_T', T)
+        np.savetxt('matrices/stereo_calib/' + prefix + '_E', E)
+        np.savetxt('matrices/stereo_calib/' + prefix + '_F', F)
+        np.savetxt('matrices/stereo_rectify/' + prefix + '_R_gauche', R1)
+        np.savetxt('matrices/stereo_rectify/' + prefix + '_R_droite', R2)
+        np.savetxt('matrices/stereo_rectify/' + prefix + '_P_gauche', P1)
+        # np.savetxt('matrices/stereo_rectify/' + prefix + '_P_droite', P2)
+        np.savetxt('matrices/stereo_rectify/' + prefix + '_Q', Q)
+        np.savetxt('matrices/points/points3D/' + prefix + '_points_3d', points3D_bis)
+
+    if show:
+        if len(rvec_gauche) == 0 | len(rvec_droite) == 0 | len(tvec_gauche) == 0 | len(tvec_droite) == 0:
+            return print('Veuillez donner les vecteurs de rotation et translation gauche et droite')
+        ax = plt.axes(projection='3d')
+        ax.scatter3D(points3D.T[0, :], points3D.T[1, :], points3D.T[2, :], 'blue')
+        plt.show()
+
+        img_gauche = cv2.drawFrameAxes(img_gauche, camera_matrix_gauche, dist_coeffs_gauche, rvec_gauche, tvec_gauche,
+                                       100)
+        img_points2, jacobian = cv2.projectPoints(obj_points, rvec_gauche, tvec_gauche,
+                                                  camera_matrix_gauche, dist_coeffs_gauche)
+        for px in img_points2:
+            cv2.circle(img_gauche, (px[0][0], px[0][1]), 10, (255, 255, 0), 5)
+        img_droite = cv2.drawFrameAxes(img_droite, camera_matrix_droite, dist_coeffs_droite, rvec_droite, tvec_droite,
+                                       100)
+        img_points3, jacobian = cv2.projectPoints(obj_points, rvec_droite, tvec_droite,
+                                                  camera_matrix_droite, dist_coeffs_droite)
+        for px in img_points3:
+            cv2.circle(img_droite, (px[0][0], px[0][1]), 10, (0, 0, 255), 5)
+
+        cv2.namedWindow('image gauche', cv2.WINDOW_NORMAL)
+        cv2.imshow('image gauche', img_gauche)
+        cv2.namedWindow('image droite', cv2.WINDOW_NORMAL)
+        cv2.imshow('image droite', img_droite)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    return camera_matrix_gauche, camera_matrix_droite, dist_coeffs_gauche, dist_coeffs_droite, R, T, E, F
+
+
+img_gauche = cv2.imread('images/piste_camera_gauche695.jpg')
+img_droite = cv2.imread('images/piste_camera_droite548.jpg')
+
+obj_points = np.loadtxt('matrices/points/points_objet/stereo_1_obj_points')
+img_points_gauche = np.loadtxt('matrices/points/points_image/stereo_1_gauche_img_points')
+img_points_droite = np.loadtxt('matrices/points/points_image/stereo_1_droite_img_points')
+
+camera_matrix_gauche = np.loadtxt('matrices/camera_matrix/extrinsic/stereo_1_gauche_camera_matrix')
+camera_matrix_droite = np.loadtxt('matrices/camera_matrix/extrinsic/stereo_1_droite_camera_matrix')
+dist_coeffs_gauche = np.loadtxt('matrices/vectors/distortion/extrinsic/stereo_1_gauche_distortion_vector')
+dist_coeffs_droite = np.loadtxt('matrices/vectors/distortion/extrinsic/stereo_1_droite_distortion_vector')
+
+rvec_gauche = np.loadtxt('matrices/vectors/rotation/stereo_1_gauche_rotation_vector')
+rvec_droite = np.loadtxt('matrices/vectors/rotation/stereo_1_droite_rotation_vector')
+tvec_gauche = np.loadtxt('matrices/vectors/translation/stereo_1_gauche_translation_vector')
+tvec_droite = np.loadtxt('matrices/vectors/translation/stereo_1_droite_translation_vector')
+
+
+find_3d_coords_stereo(img_gauche, img_droite, obj_points, img_points_gauche, img_points_droite, camera_matrix_gauche,
+                      camera_matrix_droite, dist_coeffs_gauche, dist_coeffs_droite, True, True, 'stereo_1',
+                      rvec_gauche, tvec_gauche, rvec_droite, tvec_droite)
---
+++ performance400/find_extrinsic_parameters.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+
+
+
+def find_extrinsic_parameters(img, obj_points, img_points, camera_matrix, dist_coeffs, show=True, save=False,
+                              prefix=''):
+    if save:
+        if prefix == '':
+            return print('Veuillez saisir un prefix')
+        np.savetxt('matrices/points/points_image/' + prefix + '_img_points', img_points)
+        np.savetxt('matrices/points/points_objet/' + prefix + '_obj_points', obj_points)
+
+    obj_points = np.array(obj_points, 'float32')
+    img_points = np.array(img_points, 'float32')
+    size = img.shape[:2]
+
+    retval, camera_matrix, dist_coeffs, rvecs, tvecs = cv2.calibrateCamera([obj_points], [img_points], size,
+                                                                           camera_matrix,
+                                                                           dist_coeffs)
+
+    (rotation_matrix, _) = cv2.Rodrigues(rvecs[0])
+
+    if show:
+        mean_error = 0
+        # dessiner les axes
+        img = cv2.drawFrameAxes(img, camera_matrix, dist_coeffs, rvecs[0], tvecs[0], 100)
+
+        # redessiner les points source et calculer le pourcentage d'erreur comis
+        img_points2, jacobian = cv2.projectPoints(obj_points, rvecs[0], tvecs[0], camera_matrix, dist_coeffs)
+        for px in img_points2:
+            cv2.circle(img, (px[0][0], px[0][1]), 10, (255, 255, 255), 5)
+        cv2.namedWindow("errors", cv2.WINDOW_NORMAL)
+        cv2.imshow('errors', img)
+        cv2.waitKey(0)
+        for i in range(0, len(img_points2)):
+            mean_error += cv2.norm(img_points[i], img_points2[i][0], cv2.NORM_L2) / len(img_points2)
+        print('erreur moyenne', mean_error)
+    if save:
+        np.savetxt('matrices/camera_matrix/extrinsic/'+prefix+'_camera_matrix', camera_matrix)
+        np.savetxt('matrices/vectors/distortion/extrinsic/' + prefix + '_distortion_vector', dist_coeffs)
+        np.savetxt('matrices/vectors/rotation/' + prefix + '_rotation_vector', rvecs[0])
+        np.savetxt('matrices/vectors/translation/' + prefix + '_translation_vector', tvecs[0])
+        np.savetxt('matrices/rotation_matrix/' + prefix + '_rotation_matrix', rotation_matrix)
+
+    return camera_matrix, dist_coeffs, rotation_matrix, rvecs[0], tvecs[0]
+
+
+obj_points = np.loadtxt('matrices/points/points_objet/stereo_1_obj_points')
+img_points = np.loadtxt('matrices/points/points_image/stereo_1_gauche_img_points')
+img = cv2.imread('images/piste_camera_gauche695.jpg')
+camera_matrix = np.loadtxt('matrices/camera_matrix/intrinsic/stereo_1_gauche_camera_matrix')
+dist_coeffs = np.loadtxt('matrices/vectors/distortion/intrinsic/stereo_1_gauche_distortion_vector')
+
+
+find_extrinsic_parameters(img, obj_points, img_points, camera_matrix, dist_coeffs, True, True, 'stereo_1_gauche')
