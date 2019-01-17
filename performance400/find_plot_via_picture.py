a5753fe150b346b0395084f2cc3418e6fb5832c1
yuuteng <yuuteng@foxmail.com>
2019-01-17 17:11:35 +0100

    find plot via picture save data

--- performance400/find_plot_via_picture.py
+++ performance400/find_plot_via_picture.py
@@ -11,8 +11,11 @@ result = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
 loc = np.where(result >= 0.7)
 
 for pt in zip(*loc[::-1]):
-    print(pt)  #save pt
+    print(pt)
     cv2.rectangle(img, pt, (pt[0] + width, pt[1] + height), (0, 255, 0), 1)
+
+# save object
+np.savetxt('matrices/points/points_objet/find_objet', loc)
 
 cv2.namedWindow("img", cv2.WINDOW_NORMAL)
 cv2.imshow("img", img)
