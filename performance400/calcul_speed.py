a5bd0ee022be9a049c0f478e1d9ffe17ffd306f0
Julien <julien.fallon@imt-atlantique.net>
2019-01-19 00:12:33 +0100

    tests de recreation 3D coureur

--- performance400/calcul_speed.py
+++ performance400/calcul_speed.py
@@ -1,20 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from performance400.find_3D_coords_stereo import trajectory_wrong_points
-import scipy
+import scipy.signal
+from performance400.find_3D_coords_stereo import get_wrong_points_3d
+from performance400.find_3D_coords_stereo import delete_wrong_points
+from performance400.find_3D_coords_stereo import points_filtering
 
 VIDEO_REFRESH_RATE = 30
 points_3d = np.loadtxt('matrices/points/points3D/stereo_1_points_3d')
-a=np.load('matrices/points/positions/stereo_1_droite_positions.npy')
-print(a)
-indexinterdit = trajectory_wrong_points(points_3d)
-count = 0
-indexinterdit = np.sort(indexinterdit)
+
+ind_none = get_wrong_points_3d(points_3d)
+points_3d = delete_wrong_points(points_3d, ind_none)
+points_filtering(points_3d)
 
 velocity = [np.linalg.norm(np.asarray(points_3d[i - 1, :1]) - np.asarray(points_3d[i + 1, :1])) * VIDEO_REFRESH_RATE / 2
             for i in range(1, len(points_3d) - 1)]
-
-velocity = scipy.signal.savgol_filter(velocity, 21, 5)
 
 d = 0
 D = [d]
@@ -24,18 +23,36 @@ for i in range(0, len(points_3d) - 1):
     d += ld[-1]
     D.append(d)
 
-D = scipy.signal.savgol_filter(D, 21, 5)
 
-for i in indexinterdit:
-    points_3d = np.delete(points_3d, (i - count), axis=0)
-    count += 1
+for z in range(len(ind_none)):
+    ind_none[z] -= z
+set = dict([(k, ind_none.count(k)) for k in set(ind_none)])
+sum = 0
+print(set)
+for j in range(len(velocity)):
+    if set.get(j + 1) is not None:
+        sum += set.get(j + 1)
+    if set.get(j + 2) is not None:
+        sum += set.get(j + 2)
+    if sum is not None and sum > 0:
+        velocity[j] = velocity[j] * 2 / (sum)
+    sum = 0
+
+
+velocity = np.array(velocity, 'float32')
+D = np.array(D, 'float32')
+
+velocity = velocity*3.6
+
+velocity = scipy.signal.savgol_filter(velocity, 21, 5)
+D = scipy.signal.savgol_filter(D, 21, 5)
 
 print('moyenne vitesses', np.mean(velocity))
 print('ecart type velocity', np.std(velocity))
 print('moyenne distances', np.mean(ld))
 print('ecart type disctances', np.std(ld))
 
-
-
-plt.plot(D[1: len(D) - 1], velocity)
+plt.plot(D[1: len(D) - 1], velocity, marker='+')
+plt.xlabel("Distance parcourue en m")
+plt.ylabel("Vitesse en km/h")
 plt.show()
--- performance400/find_3D_coords_stereo.py
+++ performance400/find_3D_coords_stereo.py
@@ -6,46 +6,48 @@ import math as m
 import scipy.signal
 
 
-# todo refactoriser cette merde (/!\ ce n'est pas les meme fonctions que motion detection)
-def trajectory_wrong_points(m_trajectory, m_window_length=10, m_threshold=3):
-    m_consecutive_squared_distances = [0]
-    for j in range(1, len(m_trajectory)):
-        m_consecutive_squared_distances.append(
-            (m_trajectory[j][0] - m_trajectory[j - 1][0]) ** 2 + (m_trajectory[j][1] - m_trajectory[j - 1][1]) ** 2 + (
-                    m_trajectory[j][2] - m_trajectory[j - 1][2]) ** 2)
-    m_corrected_trajectory_missing_index = []
-    for j in range(m_window_length, len(m_trajectory) - m_window_length):
-        if (m_trajectory[j][0] - m_trajectory[j - 1][0]) ** 2 + (
-                m_trajectory[j][1] - m_trajectory[j - 1][1]) ** 2 + (
-                m_trajectory[j][2] - m_trajectory[j - 1][2] ** 2) < m_threshold * np.mean(
-            m_consecutive_squared_distances[j - m_window_length:j + m_window_length]) and (m_trajectory[j][2] > -2.5):
-            continue
-        else:
-            m_corrected_trajectory_missing_index.append(j)
-    for j in range(m_window_length):
-        if (m_trajectory[j][0] - m_trajectory[j - 1][0]) ** 2 + (
-                m_trajectory[j][1] - m_trajectory[j - 1][1]) ** 2 + (
-                m_trajectory[j][2] - m_trajectory[j - 1][2] ** 2) < m_threshold * np.mean(
-            m_consecutive_squared_distances[j:j + 2 * m_window_length]) and (m_trajectory[j][2] > -2.5):
-            continue
-        else:
-            m_corrected_trajectory_missing_index.append(j)
-    for j in range(len(m_trajectory) - m_window_length, len(m_trajectory)):
-        if (m_trajectory[j][0] - m_trajectory[j - 1][0]) ** 2 + (
-                m_trajectory[j][1] - m_trajectory[j - 1][1]) ** 2 + (
-                m_trajectory[j][2] - m_trajectory[j - 1][2] ** 2) < m_threshold * np.mean(
-            m_consecutive_squared_distances[j - 2 * m_window_length:j]) and (m_trajectory[j][2] > -2.5):
-            continue
-        else:
-            m_corrected_trajectory_missing_index.append(j)
+def get_wrong_points_3d(points_3d):
+    ind_none = []
+    for n in range(np.shape(points_3d)[0]):
+        if abs(points_3d[n][0]) > 1000 or abs(points_3d[n][1]) > 1000 or abs(points_3d[n][2]) > 2.5:
+            print(points_3d[n][2], 'n: ', n)
+            ind_none.append(n)
 
-    return m_corrected_trajectory_missing_index
+    return ind_none
 
 
-def trajectory_filtering(m_trajectory):
-    m_trajectory.T[0] = scipy.signal.savgol_filter(m_trajectory.T[0], 21, 5)
-    m_trajectory.T[1] = scipy.signal.savgol_filter(m_trajectory.T[1], 21, 5)
-    m_trajectory.T[2] = scipy.signal.savgol_filter(m_trajectory.T[2], 21, 5)
+def get_positions_fails(positions_gauche, positions_droite):
+    ind_fails = []
+    for n in range(np.shape(positions_gauche)[0]):
+        if abs(positions_gauche[n][0]) > 1e16 or abs(positions_gauche[n][1]) > 1e16 \
+                or abs(positions_droite[n][0]) > 1e16 or abs(positions_droite[n][1]) > 1e16:
+            ind_fails.append(n)
+
+    return ind_fails
+
+
+def delete_wrong_points(points_3d, ind_none):
+    count = 0
+    for k in ind_none:
+        points_3d = np.delete(points_3d, (k - count), axis=0)
+        count += 1
+
+    return points_3d
+
+
+def delete_positions_fails(positions_gauche, positions_droite, ind_fails):
+    count = 0
+    for k in ind_fails:
+        positions_gauche = np.delete(positions_gauche, (k - count), axis=0)
+        positions_droite = np.delete(positions_droite, (k - count), axis=0)
+        count += 1
+    return positions_gauche, positions_droite
+
+
+def points_filtering(points_3d):
+    points_3d.T[0] = scipy.signal.savgol_filter(points_3d.T[0], 21, 5)
+    points_3d.T[1] = scipy.signal.savgol_filter(points_3d.T[1], 21, 5)
+    points_3d.T[2] = scipy.signal.savgol_filter(points_3d.T[2], 21, 5)
 
 
 def find_3d_coords_stereo(img_gauche, img_droite, obj_points, img_points_gauche, img_points_droite,
@@ -58,24 +60,6 @@ def find_3d_coords_stereo(img_gauche, img_droite, obj_points, img_points_gauche,
     img_points_droite = np.array(img_points_droite, 'float32')
     size = img_gauche.shape[:2]
 
-    #
-    # R1 = np.zeros(shape=(3, 3))
-    # R2 = np.zeros(shape=(3, 3))
-    # P1 = np.zeros(shape=(3, 3))
-    # P2 = np.zeros(shape=(3, 3))
-    #
-    # stereo_flag = cv2.CALIB_FIX_INTRINSIC
-    # retval, camera_matrix_gauche, dist_coeffs_gauche, \
-    # camera_matrix_droite, dist_coeffs_droite, R, T, E, F = cv2.stereoCalibrate([obj_points], [img_points_gauche],
-    #                                                                            [img_points_droite],
-    #                                                                            camera_matrix_gauche, dist_coeffs_gauche,
-    #                                                                            camera_matrix_droite, dist_coeffs_droite,
-    #                                                                            size, flags=stereo_flag)
-    #
-    # R1, R2, P1, P2, Q, validPixROI1, validPixROI2 = cv2.stereoRectify(camera_matrix_gauche, dist_coeffs_gauche,
-    #                                                                   camera_matrix_droite, dist_coeffs_droite,
-    #                                                                   size, R, T, R1, R2, P1, P2,
-    #                                                                   cv2.CALIB_ZERO_DISPARITY, alpha=1)
     tvec_gauche = np.array([tvec_gauche])
     tvec_droite = np.array([tvec_droite])
     m1 = np.append(rotation_matrix_gauche, tvec_gauche.T, axis=1)
@@ -84,68 +68,34 @@ def find_3d_coords_stereo(img_gauche, img_droite, obj_points, img_points_gauche,
     projection_matrix_1 = camera_matrix_gauche @ m1
     projection_matrix_2 = camera_matrix_droite @ m2
 
-    # print('Q')
-    # print(Q)
-    # print('P1_1')
-    # print(projection_matrix_1)
-    # print('P1_2')
-    # print(P1)
-    # print('P2_1')
-    # print(projection_matrix_2)
-    # print('P2_2')
-    # print(P2)
-    ind_none = []
-    count = 0;
-    for n in range(len(positions_gauche)):
-        if positions_gauche[n].__contains__(n) or positions_gauche[n].__contains__(n):
-            ind_none.append(n)
-    for k in ind_none:
-        np.delete(positions_gauche, k - count)
-        np.delete(positions_gauche, k - count)
-        count += 1;
+    ind_fail = get_positions_fails(positions_gauche, positions_droite)
 
-    points4D = cv2.triangulatePoints(projection_matrix_1, projection_matrix_2, positions_gauche.T,
-                                     positions_droite.T)
-    points3D = cv2.convertPointsFromHomogeneous(points4D.T)
+    positions_gauche, positions_droite = delete_positions_fails(positions_gauche, positions_droite, ind_fail)
+    # positions_gauche.T[0] = scipy.signal.savgol_filter(positions_gauche.T[0], 21, 5)
+    # positions_gauche.T[1] = scipy.signal.savgol_filter(positions_gauche.T[1], 21, 5)
+    # positions_droite.T[0] = scipy.signal.savgol_filter(positions_droite.T[0], 21, 5)
+    # positions_droite.T[1] = scipy.signal.savgol_filter(positions_droite.T[1], 21, 5)
 
-    points3D_bis = []
-    for p in points3D:
-        points3D_bis.append(p[0])
+    points_4d = cv2.triangulatePoints(projection_matrix_1, projection_matrix_2, positions_gauche.T,
+                                      positions_droite.T)
+    points_3d = cv2.convertPointsFromHomogeneous(points_4d.T)
 
-    points3D_bis = np.asarray(points3D_bis)
+    points_3d_bis = []
+    for p in points_3d:
+        points_3d_bis.append(p[0])
+    points_3d_bis = np.asarray(points_3d_bis)
 
-    trajectory_filtering(points3D_bis)
-
-    if save:
-        if prefix == '':
-            return print('Veuillez saisir un prefix')
-        # np.savetxt('matrices/camera_matrix/stereo_calib/' + prefix + '_gauche_camera_matrix', camera_matrix_gauche)
-        # np.savetxt('matrices/camera_matrix/stereo_calib/' + prefix + '_droite_camera_matrix', camera_matrix_droite)
-        # np.savetxt('matrices/vectors/distortion/stereo_calib/' + prefix + '_gauche_distortion_vector',
-        #            dist_coeffs_gauche)
-        # np.savetxt('matrices/vectors/distortion/stereo_calib/' + prefix + '_droite_distortion_vector',
-        #            dist_coeffs_droite)
-        # np.savetxt('matrices/stereo_calib/' + prefix + '_R', R)
-        # np.savetxt('matrices/stereo_calib/' + prefix + '_T', T)
-        # np.savetxt('matrices/stereo_calib/' + prefix + '_E', E)
-        # np.savetxt('matrices/stereo_calib/' + prefix + '_F', F)
-        # np.savetxt('matrices/stereo_rectify/' + prefix + '_R_gauche', R1)
-        # np.savetxt('matrices/stereo_rectify/' + prefix + '_R_droite', R2)
-        # np.savetxt('matrices/stereo_rectify/' + prefix + '_P_gauche', P1)
-        # np.savetxt('matrices/stereo_rectify/' + prefix + '_P_droite', P2)
-        # np.savetxt('matrices/stereo_rectify/' + prefix + '_Q', Q)
-        np.savetxt('matrices/points/points3D/' + prefix + '_points_3d', points3D_bis)
+    ind_none = get_wrong_points_3d(points_3d_bis)
 
     if show:
         img_gauche = cv2.drawFrameAxes(img_gauche, camera_matrix_gauche, dist_coeffs_gauche, rvec_gauche, tvec_gauche,
                                        3)
-        img_points2, jacobian = cv2.projectPoints(points3D_bis, rvec_gauche, tvec_gauche,
+        img_points2, jacobian = cv2.projectPoints(points_3d_bis, rvec_gauche, tvec_gauche,
                                                   camera_matrix_gauche, dist_coeffs_gauche)
-        indexinterdit = trajectory_wrong_points(points3D_bis)
 
         for j in range(len(img_points2)):
             if 0 < img_points2[j][0][0] < size[1] and 0 < img_points2[j][0][1] < size[0]:
-                if indexinterdit.__contains__(j):
+                if ind_none.__contains__(j):
                     cv2.circle(img_gauche, (m.floor((img_points2[j])[0][0]), m.floor((img_points2[j])[0][1])), 3,
                                (0, 0, 255), 20)
                 else:
@@ -154,12 +104,12 @@ def find_3d_coords_stereo(img_gauche, img_droite, obj_points, img_points_gauche,
 
         img_droite = cv2.drawFrameAxes(img_droite, camera_matrix_droite, dist_coeffs_droite, rvec_droite, tvec_droite,
                                        3)
-        img_points3, jacobian = cv2.projectPoints(points3D_bis, rvec_droite, tvec_droite,
+        img_points3, jacobian = cv2.projectPoints(points_3d_bis, rvec_droite, tvec_droite,
                                                   camera_matrix_droite, dist_coeffs_droite)
 
         for j in range(len(img_points3)):
             if 0 < img_points3[j][0][0] < size[1] and 0 < img_points3[j][0][1] < size[0]:
-                if indexinterdit.__contains__(j):
+                if ind_none.__contains__(j):
                     cv2.circle(img_droite, (m.floor(img_points3[j][0][0]), m.floor(img_points3[j][0][1])),
                                3, (0, 0, 255), 20)
                 else:
@@ -173,17 +123,30 @@ def find_3d_coords_stereo(img_gauche, img_droite, obj_points, img_points_gauche,
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-        if show:
-            if len(rvec_gauche) == 0 or len(rvec_droite) == 0 or len(tvec_gauche) == 0 or len(tvec_droite) == 0:
-                return print('Veuillez donner les vecteurs de rotation et translation gauche et droite')
-            fig = plt.figure()
-            ax = fig.add_subplot(111, projection='3d')
-            ax.scatter3D(points3D_bis.T[0, :], points3D_bis.T[1, :], points3D_bis.T[2, :], 'blue')
-            ax.set_xlabel('X (m)')
-            ax.set_ylabel('Y (m)')
-            ax.set_zlabel('Z (m)')
-            plt.show()
-    return points3D_bis
+    points_3d_bis = delete_wrong_points(points_3d_bis, ind_none)
+    if show:
+        if len(rvec_gauche) == 0 or len(rvec_droite) == 0 or len(tvec_gauche) == 0 or len(tvec_droite) == 0:
+            return print('Veuillez donner les vecteurs de rotation et translation gauche et droite')
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter3D(points_3d_bis.T[0, :], points_3d_bis.T[1, :], points_3d_bis.T[2, :], 'blue')
+        ax.set_xlabel('X (m)')
+        ax.set_ylabel('Y (m)')
+        ax.set_zlabel('Z (m)')
+        plt.show()
+    for s in ind_none:
+        points_3d_bis = np.append(np.append(points_3d_bis[:s], [[1e17, 1e17, 1e17]], axis=0),
+                                  points_3d_bis[s:], axis=0)
+    for x in ind_fail:
+        points_3d_bis = np.append(np.append(points_3d_bis[:x], [[1e17, 1e17, 1e17]], axis=0),
+                                  points_3d_bis[x:], axis=0)
+    print(ind_fail)
+    print(ind_none)
+    if save:
+        if prefix == '':
+            return print('Veuillez saisir un prefix')
+        np.savetxt('matrices/points/points3D/' + prefix + '_points_3d', points_3d_bis)
+    return points_3d_bis
 
 
 img_gauche = cv2.imread('images/piste_camera_gauche0.jpg')
@@ -206,11 +169,11 @@ tvec_droite = np.loadtxt('matrices/vectors/translation/stereo_1_droite_translati
 rotation_matrix_gauche = np.loadtxt('matrices/rotation_matrix/stereo_1_gauche_rotation_matrix')
 rotation_matrix_droite = np.loadtxt('matrices/rotation_matrix/stereo_1_droite_rotation_matrix')
 
-positions_gauche = np.loadtxt('matrices/points/positions/stereo_1_droite_positions')
+positions_gauche = np.loadtxt('matrices/points/positions/stereo_1_gauche_positions')
 positions_droite = np.loadtxt('matrices/points/positions/stereo_1_droite_positions')
 
 find_3d_coords_stereo(img_gauche, img_droite, obj_points, img_points_gauche, img_points_droite, camera_matrix_gauche,
                       camera_matrix_droite, dist_coeffs_gauche, dist_coeffs_droite, rotation_matrix_gauche,
                       rotation_matrix_droite, positions_gauche=positions_gauche, positions_droite=positions_droite,
-                      show=True, save=True, prefix='stereo_1', rvec_gauche=rvec_gauche, tvec_gauche=tvec_gauche,
+                      show=False, save=True, prefix='stereo_1', rvec_gauche=rvec_gauche, tvec_gauche=tvec_gauche,
                       rvec_droite=rvec_droite, tvec_droite=tvec_droite)
