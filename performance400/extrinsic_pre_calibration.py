ac6a3eb50a11d3a7ca5cd7caffb9b4346924079a
Thomas Schillaci <thomas.schillaci@gmail.com>
2019-01-22 13:49:19 +0100

    Project refactor

--- performance400/extrinsic_pre_calibration.py
+++ performance400/extrinsic_pre_calibration.py
@@ -1,8 +1,38 @@
+import cv2 as cv
 import numpy as np
 
 
+def draw_circle(event, x, y, flags, param):
+    if event == cv.EVENT_LBUTTONUP:
+        image = param[0]
+        param[1].append((x, y))
+        cv.circle(image, (x, y), 9, (255, 0, 255), 4)
+
+    return
+
+
+def calibrate(image):
+    name = "Pre-calibration extrinsèque"
+    cv.namedWindow(name, cv.WINDOW_NORMAL)
+    image_points = []
+    cv.setMouseCallback(name, draw_circle, param=[image, image_points])
+
+    while True:
+        cv.imshow(name, image)
+        key = cv.waitKey(1)
+        if key == ord('q') or key == ord('k') or key == 13:
+            break
+
+    cv.destroyAllWindows()
+
+    return image_points
+
+
 def calibrate(left_background, right_background, left_object_points, right_object_points):
-    pass  # left_interest_points, right_interest_points
+    left_interest_points = [calibrate(left_background), left_object_points]
+    right_interest_points = [calibrate(right_background), right_object_points]
+    # TODO reprise ici: enregistrer les poi
+    return
 
 
 def get_interest_points():
--- performance400/main.py
+++ performance400/main.py
@@ -1,9 +1,12 @@
 import glob
 import cv2 as cv
 import numpy as np
-from performance400 import intrinsic_pre_autocalibration, extrinsic_pre_calibration, extrinsic_calibration
+from matplotlib import pyplot
+from performance400 import intrinsic_pre_autocalibration, extrinsic_pre_calibration, extrinsic_calibration, \
+    trajectory_utils, speed_utils
 
 PRE_PROCESS = False
+REFRESH_RATE = 30
 
 left_video = cv.VideoCapture("videos/runway/left_run.mkv")
 right_video = cv.VideoCapture("videos/runway/right_run.mkv")
@@ -21,6 +24,28 @@ if PRE_PROCESS:
                                         left_object_points,
                                         right_object_points)
 
-interest_points = extrinsic_pre_calibration.get_interest_points()
+left_interest_points, right_interest_points = extrinsic_pre_calibration.get_interest_points()
 intrinsic_parameters = intrinsic_pre_autocalibration.get_intrinsic_parameters()
-extrinsic_calibration.calibrate(left_background, right_background, interest_points, intrinsic_parameters)
+extrinsic_calibration.calibrate(left_background, right_background, left_interest_points, right_interest_points,
+                                intrinsic_parameters)
+extrinsic_parameters = extrinsic_calibration.get_extrinsic_parameters()
+
+trajectory = trajectory_utils.get_trajectory(left_video, right_video, extrinsic_parameters)
+trajectory_utils.draw_trajectory(left_background, trajectory_utils, False, extrinsic_parameters)
+trajectory_utils.draw_trajectory(right_background, trajectory_utils, True, extrinsic_parameters)
+extrinsic_calibration.draw_axes(left_background, False)
+extrinsic_calibration.draw_axes(right_background, True)
+
+cv.namedWindow("Trajectoire de gauche", cv.WINDOW_NORMAL)
+cv.namedWindow("Trajectoire de droite", cv.WINDOW_NORMAL)
+cv.imshow("Trajectoire de gauche", left_background)
+cv.imshow("Trajectoire de droite", right_background)
+
+cv.waitKey(0)
+
+speed_profile = speed_utils.get_speed_profile(trajectory_utils, REFRESH_RATE)
+pyplot.title("Profil de vitesse")
+pyplot.xlabel("Distance (m)")
+pyplot.ylabel("Vitesse (m/s)")
+pyplot.plot(speed_profile)
+pyplot.show()
